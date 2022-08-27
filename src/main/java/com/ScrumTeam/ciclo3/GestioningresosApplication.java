2c6e911add35f2da0093cde55f7b5e7fcae609e0
Andres Sanchez <fandressanchezm@gmail.com>
2022-08-27 14:11:46 -0500

    Initial commit

---
+++ src/main/java/com/ScrumTeam/ciclo3/GestioningresosApplication.java
@@ -0,0 +1,13 @@
+package com.ScrumTeam.ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioningresosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioningresosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ScrumTeam/ciclo3/GestioningresosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ScrumTeam.ciclo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioningresosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
